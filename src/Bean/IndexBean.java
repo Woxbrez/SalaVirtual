4b0da07d252ddc0b7ef232b024f525129fe25491
Daniel Soares <danielsoaresf@hotmail.com>
2018-06-07 20:50:26 -0300

    Adicionando projeto ao Git

---
+++ src/Bean/IndexBean.java
@@ -0,0 +1,29 @@
+package Bean;
+
+import java.util.Map;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+import javax.faces.context.FacesContext;
+
+@ManagedBean(name = "indexBean")
+@SessionScoped
+public class IndexBean {
+	private String opcao = "NADA";
+	
+	public String redirecionaPag() {
+		FacesContext fc = FacesContext.getCurrentInstance();
+		Map<String,String> params = fc.getExternalContext().getRequestParameterMap();
+		opcao =  params.get("disciplina"); 
+		return "disciplina?faces-redirect=true";
+	}
+
+	public String getOpcao() {
+		return opcao;
+	}
+
+
+	public void setOpcao(String opcao) {
+		this.opcao = opcao;
+	}
+}
